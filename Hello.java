ffc1eb476f77181bc10c9f4977c8982e68a9d1e3
manoranjan-lab <57388816+manoranjan-lab@users.noreply.github.com>
2019-11-15 15:09:06 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -6,12 +6,6 @@ public class Hello{
 			System.out.println("Thanks for the webhooks demo..");
 			System.out.println("Thanks for the webhooks demo1..");
 			System.out.println("Thanks for the webhooks demonstration..");
-			System.out.println("Thanks for the webhooks demonstration 2..");
-			System.out.println("Thanks for the webhooks demonstration 3..");
-			System.out.println("Thanks for the webhooks demonstration 4..");
-			System.out.println("Thanks for the webhooks demonstration 5..");
-			System.out.println("Thanks for the webhooks demonstration 6..");
-			System.out.println("Thanks for the webhooks demonstration 8..");
 			System.out.println("Thanks for the webhooks demonstration 9..");
 			System.out.println("Thanks for the webhooks demonstration 10..");
 			System.out.println("Thanks for the webhooks demonstration 11..");
